11ff03d4691d71f6525d79bd90ed4b492d05b18c
unknown <vincentchau72@gmail.com>
2021-05-03 21:44:56 -0700

    removed several sections (testimonials, portfolio, contact)

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -53,7 +53,7 @@ class Resume extends Component {
 
 
 
-        <div className="row skill">
+        {/* <div className="row skill">
 
           <div className="three columns header-col">
             <h1><span>Skills</span></h1>
@@ -70,7 +70,7 @@ class Resume extends Component {
               </ul>
             </div>
           </div>
-        </div>
+        </div> */}
       </section>
     );
   }
